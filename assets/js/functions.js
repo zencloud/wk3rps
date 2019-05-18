acb1a173c6221c6dd33aca0f31d19cd0c34da570
zencloud <mryanmill@gmail.com>
2019-05-17 23:56:27 -0600

    Draft update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,12 @@
 // Core Game Functions
 
+// --- Update Message Display
+const game_update_message_display = function(message) {
+    let displayContainer = document.getElementById("game-message-container");
+    displayContainer.innerHTML = message;
+}
 
-// Player has selected weapon
+// --- Player has selected weapon (CORE OPERATION)
 const game_select_weapon = function(choice) {
     
     // Take arg and assign as player choice
@@ -10,6 +15,10 @@ const game_select_weapon = function(choice) {
     // Enemy Select
     gameData.enemyChoice = getRandomInt(3);
 
+    // Set Enemy Icon
+    let iconHTML = document.getElementById("enemy-icon");
+    iconHTML.src = `assets/imgs/icons/${gameData.weaponIconEnemy[gameData.enemyChoice]}`;
+
     // Compare the two choices to define winner
     let p = gameData.playerChoice;
     let e = gameData.enemyChoice;
@@ -17,23 +26,31 @@ const game_select_weapon = function(choice) {
     // Subtract Player from Enemy and mod by 3 to get sliding window for results
     let result = ( (e-p) % 3 );
 
-    console.log(result);
+    // Setup Var
+    let messageDisplay = '';
 
+    // Determine Winner/Loser
     switch (result) {
         
         case 0:
-            console.log("Tie");
+            messageDisplay = 'Game is a tie!';
         break;
 
         case -1:
         case 2:
-            console.log("Enemy Wins");
+            messageDisplay = 'Computer wins!';
         break;
 
         case 1:
         case -2:
-            console.log("Player Wins");
+            messageDisplay = 'Player wins!';
         break;
     }
 
+    // Update Message
+    game_update_message_display(messageDisplay);
+
+    setTimeout(function() {
+        game_update_message_display("Choose a weapon.");
+    }, 1050);
 }
--- assets/js/game.js
+++ assets/js/game.js
@@ -3,9 +3,10 @@
 
 // Core Game Data Object
 gameData = {
+    weaponLibrary:      ['Shield', 'Sword', 'Water'],
+    weaponIconEnemy:    ['icon_shield_color.png', 'icon_sword_color.png', 'icon_water_color.png'],
+    weaponIconEnemy:    ['icon_shield_dark.png', 'icon_sword_dark.png', 'icon_water_dark.png'],
     
-    weaponLibrary: ['Shield', 'Sword', 'Water'],
-
     // System Wide Variables
     playerChoice: null,
 
